22559a9c3f87b97c0e9ff02444233a36ff111a4f
Wil <wilbou.adrar@gmail.com>
2026-05-11 10:54:46 +0200

    first deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
+
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+
+    site:'https://www.infirmiers-castanet.fr',
+
+    integrations: [sitemap()]
 });
